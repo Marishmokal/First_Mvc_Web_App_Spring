7abf69faf0f01d647c4a5ce489a5d31955a0c5ce
Marishmokal <marishmokal96@gmail.com>
2023-01-19 08:19:26 +0530

    First commit

---
+++ First_Mvc_Web_App_Spring/src/main/java/pro/wings/controller/HelloController.java
@@ -0,0 +1,23 @@
+package pro.wings.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public String display()
+	{
+		return "index";
+	}
+	
+	
+	@RequestMapping("/Hello")  
+	public String displayException()  
+	{  
+		
+		return "abc";  
+	}  
+	
+}
